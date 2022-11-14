42ebd9c0f80b335e33ed6d61e1a0670fdca38b4c
tuyen <20130459@st.hcmuaf.edu.vn>
2022-11-14 13:21:00 +0700

    viet chuong trinh giao tiep don gian giua client va server

--- Socket/src/thread/single/EchoChatClient.java
+++ Socket/src/thread/single/EchoChatClient.java
@@ -1,4 +1,60 @@
 package thread.single;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class EchoChatClient {
+
+    static final String SERVER_IP = "127.0.0.1";
+    static final int SERVER_PORT = 7;
+
+    public static void EchoClient() {
+
+        Socket socket = null;
+        Scanner sc = new Scanner(System.in);
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT); //connect to server
+
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            DataInputStream dis = new DataInputStream(is);
+            DataOutputStream dos = new DataOutputStream(os);
+
+            String welcome = dis.readUTF();
+            if (welcome.equals("Welcome")) {
+
+                System.out.println(welcome + " " + socket.toString());
+                while (true) {
+
+                    System.out.println("Input:");
+                    String input = sc.nextLine();
+
+                    dos.writeUTF(input);
+
+
+                    String dataFromServer = dis.readUTF();
+                    System.out.println(dataFromServer);
+                    if (dataFromServer.equalsIgnoreCase("Disconnect")) break;
+
+
+                }
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println("can't connect to server");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        EchoClient();
+
+    }
+
+
 }
--- Socket/src/thread/single/EchoChatSingleServer.java
+++ Socket/src/thread/single/EchoChatSingleServer.java
@@ -1,4 +1,58 @@
 package thread.single;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoChatSingleServer {
+
+    static final int SERVER_PORT = 7;
+
+    public static void EchoServer() {
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT); // open Port Server -> Server is start
+            Socket socket = serverSocket.accept(); // Client is accepted
+            System.out.println(socket);
+
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+            DataInputStream dis = new DataInputStream(is);
+            DataOutputStream dos = new DataOutputStream(os);
+
+            String welcome = "Welcome";
+            dos.writeUTF(welcome);
+
+            while (true) {
+
+                String dataFromClient = dis.readUTF();
+
+
+                if (dataFromClient.equalsIgnoreCase("Exit")) {
+
+                    dos.writeUTF("Disconnect");
+                    socket.close();
+                    break;
+
+                } else {
+                    dos.writeUTF("Echo " + dataFromClient);
+                }
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("server is error");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        EchoServer();
+    }
+
+
 }
